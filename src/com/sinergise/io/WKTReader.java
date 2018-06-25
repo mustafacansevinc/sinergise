765df80c1fe650c7e939f8bea38da3b490db84b5
M. Can Sevinc <mcansevinc@gmail.com>
2018-06-25 21:26:45 +0300

    initial

---
+++ src/com/sinergise/io/WKTReader.java
@@ -0,0 +1,17 @@
+package com.sinergise.io;
+
+import com.sinergise.geometry.Geometry;
+import com.sinergise.geometry.GeometryCollection;
+import com.sinergise.geometry.LineString;
+import com.sinergise.geometry.Point;
+
+public class WKTReader {
+	
+	/**
+	 * Transforms the input WKT-formatted String into Geometry object
+	 */
+	public Geometry read(String wktString) {
+		//TODO: Implement this
+	}
+
+}
---
+++ src/com/sinergise/io/WKTWriter.java
@@ -0,0 +1,21 @@
+package com.sinergise.io;
+
+import com.sinergise.geometry.Geometry;
+import com.sinergise.geometry.GeometryCollection;
+import com.sinergise.geometry.LineString;
+import com.sinergise.geometry.Point;
+
+public class WKTWriter {
+	
+	/**
+	 * Transforms the input Geometry object into WKT-formatted String. e.g.
+	 * <pre><code>
+	 * new WKTWriter().write(new LineString(new double[]{30, 10, 10, 30, 40, 40}));
+	 * //returns "LINESTRING (30 10, 10 30, 40 40)"
+	 * </code></pre>
+	 */
+	public String write(Geometry geom) {
+		//TODO: Implement this
+		return write(new GeometryCollection<Geometry>(new Geometry[]{new Point(4,6), new LineString(new double[] {4,6,7,10})}));
+	}
+}
