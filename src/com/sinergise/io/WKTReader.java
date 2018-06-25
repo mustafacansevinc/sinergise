9d1e826d28fc56ac1261490ff10336982efe455b
M. Can Sevinc <mcansevinc@gmail.com>
2018-06-25 23:55:09 +0300

    worked on design

--- src/com/sinergise/io/WKTReader.java
+++ src/com/sinergise/io/WKTReader.java
@@ -16,50 +16,65 @@ public class WKTReader {
     public Geometry read(String wktString) {
         //TODO: Implement this
         if (wktString.startsWith("POINT")){
-            Pattern regex = Pattern.compile("([\\d.d]+)\\s([\\d.d]+)");
-            Matcher m = regex.matcher(wktString);
-            Point p = null;
-
-            if (m.find()) {
-                double x = Double.parseDouble(m.group(1));
-                double y = Double.parseDouble(m.group(2));
-                p = new Point(x, y);
-            } else {
-                p = new Point();
-            }
-            System.out.println(p.toString());
-            return p;
+            return readPoint(wktString);
         }
 
         else if (wktString.startsWith("LINESTRING")){
-            Pattern regex = Pattern.compile("([\\d.d]+)\\s([\\d.d]+)");
-            Matcher m = regex.matcher(wktString);
-            List<Double> points = new ArrayList<Double>();
-
-            while (m.find()) {
-                points.add(Double.parseDouble(m.group(1)));
-                points.add(Double.parseDouble(m.group(2)));
-            }
-
-            double[] coords = new double[points.size()];
-            int i = 0;
-            for (Double p : points) {
-                coords[i] = p;
-                i++;
-            }
-
-            System.out.println(new LineString(coords).toString());
-            return new LineString(coords);
+            return readLineString(wktString);
         }
 
         else if (wktString.startsWith("POLYGON")) {
-            Pattern regex = Pattern.compile("([\\d.d]+)\\s([\\d.d]+)");
-            Matcher m = regex.matcher(wktString);
-            List<LineString> ls = new ArrayList<LineString>();
-
-            //TODO: work on recursive function for proper read
+            return readPolygon(wktString);
         }
 
+        return null;
+    }
+    private Point readPoint(String wktString){
+        Pattern regex = Pattern.compile("([\\d.d]+)\\s([\\d.d]+)");
+        Matcher m = regex.matcher(wktString);
+        Point p = null;
+
+        if (m.find()) {
+            double x = Double.parseDouble(m.group(1));
+            double y = Double.parseDouble(m.group(2));
+            p = new Point(x, y);
+        } else {
+            p = new Point();
+        }
+        System.out.println(p.toString());
+        return p;
+    }
+
+    private LineString readLineString(String wktString){
+        Pattern regex = Pattern.compile("([\\d.d]+)\\s([\\d.d]+)");
+        Matcher m = regex.matcher(wktString);
+        List<Double> points = new ArrayList<Double>();
+
+        while (m.find()) {
+            points.add(Double.parseDouble(m.group(1)));
+            points.add(Double.parseDouble(m.group(2)));
+        }
+
+        double[] coords = new double[points.size()];
+        int i = 0;
+        for (Double p : points) {
+            coords[i] = p;
+            i++;
+        }
+
+        System.out.println(new LineString(coords).toString());
+        return new LineString(coords);
+    }
+
+    private Polygon readPolygon(String wktString){
+        Pattern regex = Pattern.compile("([\\d.d]+)\\s([\\d.d]+)");
+        Matcher m = regex.matcher(wktString);
+        List<LineString> ls = new ArrayList<LineString>();
+
+        while (m.find()) {
+            ls.add(readLineString(m.group()));
+        }
+        System.out.println(ls);
         return null;
     }
 
